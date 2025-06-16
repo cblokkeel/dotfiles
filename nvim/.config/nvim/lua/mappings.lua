b5c8907454013fbf8a89d1a0037420c26320ba42
cblokkeel <cblokkeel1@gmail.com>
2025-06-16 09:49:00 +0200

    nvim: add lsp mappings

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -32,13 +32,26 @@ map("n", "<C-S-N>", function() harpoon:list():next() end)
 -- Twilight
 map("n", "<leader>tw", ":Twilight<cr>", {})
 
+
+-- LSP
+map("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", {})
+map("n", "gd", vim.lsp.buf.definition, {})
+
+map("n", "<leader>r", vim.lsp.buf.rename, {})
+
+-- Close quickfix after jumping to a location
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "qf",
+    command = [[nnoremap <buffer> <CR> <CR>:cclose<CR>]]
+})
+
 -- Misc
-function close_all_buffers()
-    local bufs=vim.api.nvim_list_bufs()
-    local current_buf=vim.api.nvim_get_current_buf()
-    for _,i in ipairs(bufs) do
-        if i~=current_buf then
-            vim.api.nvim_buf_delete(i,{})
+local function close_all_buffers()
+    local bufs = vim.api.nvim_list_bufs()
+    local current_buf = vim.api.nvim_get_current_buf()
+    for _, i in ipairs(bufs) do
+        if i ~= current_buf then
+            vim.api.nvim_buf_delete(i, {})
         end
     end
 end
