ffa5b4b2b3dd3242e2f6a52d27b32b501b8fbc7f
cblokkeel <cblokkeel1@gmail.com>
2025-06-16 09:49:32 +0200

    nvim: add mason-lspconfig to ensure installed lsp servers

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,32 +1,35 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{
-			    "folke/lazydev.nvim",
-			    ft = "lua", -- only load on lua files
-			    opts = {
-			      library = {
-				-- See the configuration section for more details
-				-- Load luvit types when the `vim.uv` word is found
-				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-			      },
-			    },
-			},
-			{
-			  "williamboman/mason.nvim",
-			},
-		},
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            {
+                "folke/lazydev.nvim",
+                ft = "lua", -- only load on lua files
+                opts = {
+                    library = {
+                        -- See the configuration section for more details
+                        -- Load luvit types when the `vim.uv` word is found
+                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                    },
+                },
+            },
+            {
+                "williamboman/mason.nvim",
+            },
+            {
+                "williamboman/mason-lspconfig.nvim",
+            }
+        },
         opts = {
             servers = {
                 biome = {
-                    cmd = {"biome", "lsp-proxy"},
+                    cmd = { "biome", "lsp-proxy" },
                     filetypes = { "javascript", "typescript", "typescriptreact", "javascriptreact", "json", "css", "scss", "markdown" },
                     root_dir = function(fname)
                         return require("lspconfig.util").root_pattern(".biome.json", ".git")(fname)
                     end,
                     on_attach = function(client, bufnr)
-                          if client.server_capabilities.documentFormattingProvider then
+                        if client.server_capabilities.documentFormattingProvider then
                             local opts = { noremap = true, silent = true, buffer = bufnr }
                             vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
                         end
@@ -35,12 +38,25 @@ return {
                 },
             }
         },
-		config = function(_, opts)
-			local lspconfig = require("lspconfig")
-			local mason = require("mason")
-			mason.setup()
+        config = function(_, opts)
+            local lspconfig = require("lspconfig")
+            local mason = require("mason")
+            local mason_lsp_config = require("mason-lspconfig")
 
-			lspconfig.lua_ls.setup {}
+            mason.setup()
+
+            mason_lsp_config.setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "ts_ls",
+                    "vue_ls",
+                    "gopls",
+                    "dockerls",
+                },
+                automatic_enable = true,
+            })
+
+            lspconfig.lua_ls.setup {}
 
             lspconfig.ts_ls.setup {
                 init_options = {
@@ -48,7 +64,7 @@ return {
                         {
                             name = "@vue/typescript-plugin",
                             location = "/home/colin/.local/share/pnpm/global/5/node_modules/@vue-typescript-plugin",
-                            languages = {"javascript", "typescript", "vue"},
+                            languages = { "javascript", "typescript", "vue" },
                         },
                     },
                 },
@@ -88,6 +104,6 @@ return {
             for server, server_opts in pairs(opts.servers) do
                 lspconfig[server].setup(server_opts)
             end
-		end
-	}
+        end
+    }
 }
