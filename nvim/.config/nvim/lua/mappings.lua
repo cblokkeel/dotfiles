258de4b4c88f034babe335d2f146104788f0ea86
cblokkeel <cblokkeel1@gmail.com>
2025-07-05 15:20:00 +0200

    nvim: remove harpoon

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -14,21 +14,6 @@ map("n", "<C-b>", ":lua MiniFiles.close()<cr>", {})
 map("n", "<leader>l", ":bnext<cr>zz", {})
 map("n", "<leader>h", ":bprevious<cr>zz", {})
 
-local harpoon = require("harpoon")
-
--- Harpoon
-map("n", "<leader>a", function() harpoon:list():add() end)
-map("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-map("n", "<C-h>", function() harpoon:list():select(1) end)
-map("n", "<C-t>", function() harpoon:list():select(2) end)
-map("n", "<C-v>", function() harpoon:list():select(3) end)
-map("n", "<C-s>", function() harpoon:list():select(4) end)
-
--- Toggle previous & next buffers stored within Harpoon list
-map("n", "<C-S-P>", function() harpoon:list():prev() end)
-map("n", "<C-S-N>", function() harpoon:list():next() end)
-
 -- Twilight
 map("n", "<leader>tw", ":Twilight<cr>", {})
 
